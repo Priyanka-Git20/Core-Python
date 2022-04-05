6b26c6582a41017b10b5f49def96dd27bd00e154
priyanka <priyanka.chavan1408@gmail.com>
2022-04-05 20:18:27 +0530

    [Priyanka]Add:Calculate the head and tail percentage.

---
+++ basicPrograms/FlipCoin.py
@@ -0,0 +1,37 @@
+'''
+@Author : Priyanka
+@Date : 2022-04-05  12:30:00
+@Last Modified by : Priyanka
+@Last Modified Time : 2022-04-05  12:45:00
+@Title : Flip Coin and print percentage of Heads and Tails.
+'''
+
+import random
+
+
+def flipPercentage():
+    """
+        Description:
+            Calculate the percentage of heads and tails
+        Parameter:
+             none
+        Return:
+            Return the printing message
+    """
+
+    print("Enter the how many times want to flip the coin")
+    numberOfFlip = int(input())
+    tail = 0
+    head = 0
+    for i in range(1,numberOfFlip+1):
+        flipCheck = random.uniform(0,1)
+        if flipCheck <0.5 :
+            tail += 1
+        else:
+            head += 1
+    tailPercentage = (tail/numberOfFlip)*100
+    print("Tail percentage is",tailPercentage)
+    headPercentage = 100 - tailPercentage
+    print("Head percentage is",headPercentage)
+
+(flipPercentage())
