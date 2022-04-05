88ed338b2cf65616d7bdccb43a5c6dddca0a3341
priyanka <priyanka.chavan1408@gmail.com>
2022-04-05 20:22:11 +0530

    [Priyanka]Add:Compute the power of two.

---
+++ basicPrograms/PowerOfTwo.py
@@ -0,0 +1,29 @@
+'''
+@Author : Priyanka
+@Date : 2022-04-05  14:00:00
+@Last Modified by : Priyanka
+@Last Modified Time : 2022-04-05  14:35:00
+@Title : Computes the power of two.
+'''
+
+import math
+def computePowerOfTwo():
+    """
+       Description:
+           Function is used to compute the power of two.
+       Parameter:
+           None
+       Return:
+           Returning nothing but print the message.
+    """
+
+    print("Enter a Exponent")
+    exponent = int(input())
+
+    if (exponent >= 0) and (exponent < 31):
+            value = int(math.pow(2,exponent))
+            print("Value of 2^{} is {}".format(exponent,value))
+    else:
+            print("Enter the exponent between 0 to 31")
+
+computePowerOfTwo()
