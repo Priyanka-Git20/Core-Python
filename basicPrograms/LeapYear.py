6c4c3c127fb0c3a692d1112d578fb1f1d7bc88f9
priyanka <priyanka.chavan1408@gmail.com>
2022-04-06 10:48:50 +0530

    [Priyanka]refactor:Use main function and modified the code.

--- basicPrograms/LeapYear.py
+++ basicPrograms/LeapYear.py
@@ -16,20 +16,17 @@ def checkLeapYear ():
            Returning nothing but print the message.
     """
 
-    if (year % 400 == 0) and (year % 100 == 0):
-        print("{} is leap year".format(year))
-    elif (year % 4 == 0) and (year % 100 != 0):
+    if (year % 400 == 0) and (year % 100 == 0) or (year % 4 == 0):
         print("{} is leap year".format(year))
     else:
         print("{} is not a leap year".format(year))
 
+if __name__ == '__main__':
+     year = int(input(("Enter the year you want to check is it leap or not:\n")))
 
-print("Enter the year you want to check is it leap or not")
-year = int(input())
-
-if (year > 9999) or (year < 1000):
-    print("Enter the year with 4 digit number")
-else:
-    checkLeapYear()
+     if (year > 9999) or (year < 1000):
+          print("Enter the year with 4 digit number")
+     else:
+          checkLeapYear()
 
 
